2a3130e1691d093ba4d750fb0f04b5fc97ccb522
niklerios <web.nikler@yandex.ru>
2026-05-23 03:58:58 +0300

    fix(sprint_11): подгон размеров ProgressHUD под макет

--- ImageFeed/Services/LoadingService.swift
+++ ImageFeed/Services/LoadingService.swift
@@ -16,15 +16,23 @@ protocol LoadingServiceProtocol: AnyObject {
 final class LoadingService: LoadingServiceProtocol {
     static let shared = LoadingService()
     
-    private init() {}
+    private init() {
+        // В либе ProgressHUD баг скалирования (скейл x1.4 и обратный x1/1.4 применяются к исходному фрейму)
+        // из-за чего получаем некорректные размеры
+        // используем корректирующий коэффициент для компенсации ошибочной арифмеике в либе
+        // подробнее - ProgressHUD/Sources/ProgressHUD - 557 и 560 строки
+        ProgressHUD.mediaSize = 25 * 1.4
+        ProgressHUD.marginSize = 13 * 1.4
+    }
     
     func showProgress() {
-        UIApplication.shared.keyWindow?.isUserInteractionEnabled = false
-        ProgressHUD.animate()
+        // Альтернатива - дизейблить window ,но мне показалось логичнее использовать вшитый механизм в либу
+        // UIApplication.shared.keyWindow?.isUserInteractionEnabled = false
+        ProgressHUD.animate(interaction: false)
     }
     
     func hideProgress() {
-        UIApplication.shared.keyWindow?.isUserInteractionEnabled = true
+        // UIApplication.shared.keyWindow?.isUserInteractionEnabled = true
         ProgressHUD.dismiss()
     }
 }
